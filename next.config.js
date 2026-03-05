aaead5bbcf0686cfa9a74d3d7d5d41b94a4dbf3d
Prince Sahgal <sahgalanubhav3@gmail.com>
2026-03-05 12:53:36 +0530

    changes in net.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {},
   images: {
-    domains: ['m.media-amazon.com', 'ia.media-imdb.com', 'image.tmdb.org'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.media-amazon.com',
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "/images/**",
       },
     ],
   },
 };
 
+export default nextConfig;
